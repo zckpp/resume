7013c8e85882756abfb699236324ccb4d1f5863c
zckpp <chengkun.zhang@rhsmith.umd.edu>
2016-09-19 22:15:41 -0400

    update

--- js/main.js
+++ js/main.js
@@ -44,35 +44,7 @@ $(window).scroll(function() {
         offset: 80
     })
 
-  	/*====================================
-    Portfolio Isotope Filter
-    ======================================*/
-    $(window).load(function() {
-        var $container = $('.portfolio-items');
-        $container.isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-        });
-        $('.cat a').click(function() {
-            $('.cat .active').removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
-        });
 
-    });
   	/*====================================
     Pretty Photo
     ======================================*/
