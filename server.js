1d305b14722076f2055ba62ad68841f2f83989fd
Quinn Zhang <qzhang@pst.ciw.edu>
2017-05-17 13:34:52 -0400

    init

--- server.js
+++ server.js
@@ -6,6 +6,6 @@ var compression = require('compression')
 app.use(compression())
 
 var static = require('serve-static');
-app.use(static(__dirname + "/httpdocs"));
+app.use(static(__dirname + "/public"));
 
 http.createServer(app).listen(process.env.PORT || 80);
